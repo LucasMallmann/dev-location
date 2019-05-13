8cd039291b4e2c58428719d273493e41ce936db3
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-05-13 20:44:39 -0300

    Pull com o saga

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Fragment } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faStroopwafel, faTimesCircle, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
-import './config/ReactotronConfig';
-
 import GlobalStyle from './style';
 import Routes from './routes';
 
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -4,6 +4,10 @@ import { Types as UserTypes } from '../ducks/user';
 
 import { addUser } from './users';
 
+/**
+ * Combinar todos os sagas em um só
+ */
 export default function* rootSaga() {
+  // Configurar para que quando a action ADD_REQUEST disparar, chamar o saga 'addUser'
   yield all([takeLatest(UserTypes.ADD_REQUEST, addUser)]);
 }
